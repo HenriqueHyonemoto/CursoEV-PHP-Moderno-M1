cb236926896e0e93583a3ca605157d5b9b0bbe74
HenriqueHyonemoto <ifhenriquehyonemoto@gmail.com>
2024-11-15 16:21:29 -0300

    ex003

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World PHP</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "Hello World! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sunt nihil voluptatum architecto minus laudantium, incidunt ipsa magni minima aspernatur aliquid maxime itaque culpa neque ut aperiam sint! Dignissimos, odio?</p>
+</body>
+
+</html>
--- ex001/index.php
+++ ex001/index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dados do Servidor</title>
 </head>
 <body>
-    
+    <h1>Dados do Servidor</h1>
+    <?= "short echo tag" ?>
+    <?php 
+    phpinfo();
+    ?>
+
 </body>
 </html>
