a65830dda143d74e5d63abe61a000985d4618d5f
HenriqueHyonemoto <ifhenriquehyonemoto@gmail.com>
2024-11-22 10:04:16 -0300

    raizes

---
+++ 8desafios/8-raizes/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raizes</title>
+    <link rel="stylesheet" href="../../php-moderno-main/downloads/modelo-css/style.css">
+</head>
+<?php 
+$num = $_GET['num'] ?? 1;
+
+$raizQuadrada = $num ** (1/2);
+$raizCubica= $num ** (1/3);
+
+$raizQuadrada= number_format($raizQuadrada,3,',','.');
+$raizCubica= number_format($raizCubica,3,',','.');
+?>
+<body>
+    <main>
+<h1>Informe um Número</h1>
+       <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <label for="num">Número</label>
+        <input type="number" step=0.001 id="num" name="num">
+        <input type="submit" value="Calcular Raizes">
+       </form>
+    </main>
+    <section>
+        <h1>Resultado Final</h1>
+       <p>Analisando o <strong>número <?= $num ?></strong>, temos:</p>
+       <ul>
+        <li>A sua raiz quadrada é: <strong><?= $raizQuadrada ?></strong> </li>
+        <li>A sua raiz cubica é: <strong><?= $raizCubica ?></strong> </li>
+       </ul>
+
+    </section>
+</body>
+
+</html>
